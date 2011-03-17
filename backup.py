7783e2e0daacb4bb95c76e1d23e4bd6371cccfed
Queria Sa-Tas <queria@sa-tas.net>
2011-03-17 16:25:01 +0100

    usage of configuration file and actually creating backups now

--- backup.py
+++ backup.py
@@ -1,27 +1,37 @@
 #!/usr/bin/env python2.6
+import ConfigParser
 import os
 import sys
 import re
-from subprocess import check_output
+import datetime
+import subprocess
 
-#SOURCE_DIR = "/home/www/"
-#TARGET_DIR = "/.backup/"
-SOURCE_DIR = "C:\\Users\\queria\\Documents\\share_downloader"
-TARGET_DIR = "C:\\Users\\queria\\Documents\\backup"
+
+SOURCE_DIR = "/please-prepare-configuration-file"
+TARGET_DIR = SOURCE_DIR
+EXCLUDE_FILE = './backup.exclude'
+
+cfg = ConfigParser.RawConfigParser()
+cfg.read('backup.cfg')
+SOURCE_DIR = cfg.get('main', 'source_dir')
+TARGET_DIR = cfg.get('main', 'target_dir')
 
 skip_list = []
-with open('./backup.exclude', 'r') as f:
-	for line in f:
-		if line[0] == '#':
-			continue
-		skip_list.append(line.strip())
+try:
+    with open(EXCLUDE_FILE, 'r') as f:
+        for line in f:
+            if line[0] == '#':
+                continue
+            skip_list.append(line.strip())
+except IOError, e:
+    skip_list = []
 
 def warning(message):
-	print(message)
+    print(message)
 
 def error(message):
-	print(message)
-	sys.exit(1)
+    print(message)
+    sys.exit(1)
 
 _slugify_strip_re = re.compile(r'([^\w\s-]|[\s])+')
 #_slugify_hyphenate_re = re.compile(r'[_\s]+')
@@ -29,7 +39,7 @@ def slugify(value):
     """
     Normalizes string, converts to lowercase, removes non-alpha characters,
     and converts spaces to hyphens.
-    
+
     From Django's "django/template/defaultfilters.py".
     """
     import unicodedata
@@ -38,7 +48,7 @@ def slugify(value):
     value = unicodedata.normalize('NFKD', value).encode('ascii', 'ignore')
     value = unicode(_slugify_strip_re.sub('_', value).strip().lower())
     return value
-	#_slugify_hyphenate_re.sub('-', value)
+#_slugify_hyphenate_re.sub('-', value)
 
 
 result_output = ''
@@ -46,48 +56,54 @@ to_backup = []
 failed_paths = []
 
 cmd = ['tar',
-		'--create',
-		'--gzip',
-		'--preserve-permission',
-		'--one-file-system',
-		]
+        '--create',
+        '--gzip',
+        '--preserve-permission',
+        '--one-file-system',
+        ]
+if skip_list:
+    cmd += ['--exclude-from', EXCLUDE_FILE]
+
 
 for sub in os.listdir(SOURCE_DIR):
-	#if not os.path.isdir(SOURCE_DIR + sub):
-	#	not_dirs.append(SOURCE_DIR + sub)
-	#else:
-	srcdir = os.path.join(SOURCE_DIR, sub)
-	stgt = slugify(srcdir)
-	tgtdir = os.path.join(TARGET_DIR, stgt)
+    #if not os.path.isdir(SOURCE_DIR + sub):
+    #   not_dirs.append(SOURCE_DIR + sub)
+    #else:
+    srcdir = os.path.join(SOURCE_DIR, sub)
+    stgt = slugify(srcdir)
+    tgtdir = os.path.join(TARGET_DIR, stgt)
 
-	if srcdir in skip_list:
-		continue
+    if srcdir in skip_list:
+        continue
 
-	cmd_args = ['-g', os.path.join(tgtdir, '_status'),
-			'--file={0}.tar.gz'.format(os.path.join(tgtdir, stgt)),
-			srcdir]
-	
-	try:
-		result_output += "{0}\n".format(cmd + cmd_args)
-	except CalledProcessError, e:
-		result_output += "----------------------\nBackup of {0} failed:\n".format(srcdir)
-		result_output += str(e.output)
-		result_output += "----------------------\n"
-		pass
-	except OSError, e:
-		result_output += "----------------------\nBackup of {0} failed:\n".format(srcdir)
-		result_output += str(e)
-		result_output += "----------------------\n"
-		pass
-	
+    cmd_args = ['-g', os.path.join(tgtdir, '_status'),
+        '--file={0}-{1}.tar.gz'.format(
+            os.path.join(tgtdir, stgt),
+            datetime.datetime.now().strftime('%Y_%m_%d-%H_%M_%S_%f')),
+        srcdir]
 
-# TAROPTS="--gzip --preserve-permission --one-file-system"
-# if ! tar --create ${TAROPTS} -g ${QS_TGTDIR}"/_status" --file=${QS_TGTDIR}"/"${QS_TGT}"_"${STAMP}".tar.gz" ${QS_SRC} &> /dev/null;
+    try:
+        if not os.path.exists(tgtdir):
+            os.makedirs(tgtdir, 0700)
+        #result_output += "{0}\n".format(cmd + cmd_args)
+        tar = subprocess.Popen(cmd + cmd_args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        tar_out = tar.communicate()
+        if tar.returncode != 0:
+            result_output += "----------------------\nBackup of {0} failed:\n".format(srcdir)
+            result_output += str(tar_out[1])
+            result_output += "\n----------------------\n"
+    except OSError, e:
+        result_output += "----------------------\nBackup of {0} failed:\n".format(srcdir)
+        result_output += "Error #" + str(e.errno) + ": " + e.strerror
+        result_output += "\n----------------------\n"
+        pass
+
 
 if failed_paths:
-	warning("Nasledujici cesty se nepovedlo zalohovat: {0}".format(failed_paths))
+    warning("Nasledujici cesty se nepovedlo zalohovat: {0}".format(failed_paths))
 
-print skip_list
-print(result_output)
+#print skip_list
+if result_output:
+    print("_...----=======[ QSBackup starting ]=======----..._")
+    print(result_output)
 
-raw_input()
