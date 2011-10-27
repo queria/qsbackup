53a3c4d7c4d746e4cc2816e5330d142a66daf140
Queria Sa-Tas <queria@sa-tas.net>
2011-10-27 22:45:45 +0200

    finished rewrite of backup-db to ruby

--- backup-db.rb
+++ backup-db.rb
@@ -16,36 +16,52 @@ def list_databases(dbcfg={'user'=>'','pass'=>''})
 	return databases
 end
 
-def filter_databases(databases, skip_info_schema=true, skip_dbnames='')
-	databases
+def filter_databases(databases, skip_info_schema=true, skip_dbnames=[])
+	databases.select do |dbname|
+		(dbname != 'information_schema' or not skip_info_schema) \
+		and (not skip_dbnames.include? dbname)
+	end
 end
 
 def dump_db(dbcfg, dbname, backup_dir)
-	db_path = backup_dir + (dbname + '.sql')
+	db_path = backup_dir + (dbname + '.sql.bz2')
 	puts "storing #{db_path}"
-	`mysqldump --quick --disable-keys -u #{dbcfg['user']} -p#{dbcfg['pass']} #{dbname} > #{db_path.to_s}`
+	`mysqldump --quick --disable-keys -u #{dbcfg['user']} -p#{dbcfg['pass']} #{dbname} | bzip2 -c > #{db_path.to_s}`
 end
 
 config_path = './backup-db.cfg'
 config_path = ARGV[0] unless ARGV[0].nil?
 config = YAML::load_file(config_path)
+config['skip_dbs'] ||= ''
+
+db_config = {'user'=>config['user'], 'pass'=>config['pass']}
+dbs = list_databases(db_config)
+dbs = filter_databases(dbs, config['skip_info_schema'], config['skip_dbs'].split(':'))
+
+if dbs.empty?
+	puts 'No database to backup found!'
+	exit 0
+end
 
 backup_dir = Pathname.new(config['backup_dir'])
 unless backup_dir.directory?
-	# or backup_dir.mkpath
-	# -- dont create it to save troubles with typos ;)
-	# -- let the user handle corner cases
 	puts 'Backup directory does not exist!'
 	exit 1
 end
 
-db_config = {'user'=>config['user'], 'pass'=>config['pass']}
-dbs = list_databases(db_config)
-dbs = filter_databases(dbs, config['skip_info_schema'], config['skip_dbs'])
+stamp = Time.new.strftime('%Y%m%d-%H%M%S')
+backup_dir += stamp
+backup_dir.mkpath
+unless backup_dir.directory?
+	puts "Unable to write into backup dir (#{backup_dir})!"
+	exit 2
+end
 
 dbs.each do |db|
 	dump_db(db_config, db, backup_dir)
 end
 
 puts 'done'
+
+exit 0
 
